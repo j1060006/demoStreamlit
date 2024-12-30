51ec29dc34e42cb1e56b147f3fc38ac268bb6eef
j1060006 <j1060006@gmail.com>
2024-12-30 23:13:43 +0800

    add

--- demoStreamlit.py
+++ demoStreamlit.py
@@ -15,7 +15,7 @@ st.set_page_config(page_title="Stock Buy & Hold", page_icon="📈")
 st.header('繪製個股Buy & Hold策略')
 syb = ['SPY', 'QQQ', 'DIA', 'IWM', 'VOO', 'VT', 'VTI', 'TLT', 'SMH', 'BND', 'SOXX', 'VXUS']
 stockId = st.selectbox('選擇個股', syb)
-
+initCapital = st.number_input('起始投入資金', min_value=0, step=100)
 left_column, right_column = st.columns(2)
 startDate = left_column.date_input('回測起始日')
 endDate = right_column.date_input('回測終止日')
@@ -27,28 +27,36 @@ if btn:
     df = yf.Ticker(stockId).history(start = startDate, end = endDate, interval = '1d', auto_adjust=False)
     df.index = [i.date() for i in df.index]
     df.index.name = 'Date' 
-    dd = []
-    hh = 0
-    for i in range(df.shape[0]):
-        if df.loc[df.index[i], 'Adj Close'] > hh:
-            hh = df.loc[df.index[i], 'Adj Close']
-        dd.append(df.loc[df.index[i],'Adj Close']-hh)
-    df.insert(6, 'DrowDown', dd)  
     
-    df['logReturn'] = np.log(df['Adj Close'] / df['Adj Close'].shift(1))  
-  
-        
+    
+    df['logReturn'] = np.log(df['Adj Close'] / df['Adj Close'].shift(1))          
     df['logReturn'] = df['logReturn'].fillna(0)
-    
+    df['cumLogReturn'] = df['logReturn'].cumsum(axis=0)
     avg = df['logReturn'].mean() * 252 * 100
     std = np.sqrt(252) * np.std(df['logReturn']) * 100
-    mdd = df['DrowDown'].min()
-    res_col_left, res_col_mid,  res_col_right= st.columns(3)
-    res_col_left.metric('平均年化報酬率', value="{} %".format(np.round(avg, 2)))    
-    res_col_mid.metric('平均年化波動率', value="{} %".format(np.round(std, 2)))    
-    res_col_right.metric('持倉期間最大拉回 / 股', value="{} USD".format(np.round(mdd, 2)))
+    
 
-    df = df.round(2)
+    dd = []
+    for i in range(df.shape[0]):
+        hh = df.loc[df.index[:i+1], 'logReturn'].max()
+        dd.append(df.loc[df.index[i], 'logReturn'] - hh)
+    df['DrowDown'] = dd  
+    mdd = (df['DrowDown'].min()) * 100    
+    df['ProfitLoss'] = initCapital * df['logReturn'].cumsum(axis=0)
+
+    ddc = []
+    for i in range(df.shape[0]):
+        hhc = df.loc[df.index[:i+1], 'Adj Close'].max()
+        ddc.append(df.loc[df.index[i], 'Adj Close'] - hhc)
+    df['DrowDownC'] = ddc  
+
+
+    a1, a2, a3, a4= st.columns(4)
+    a1.metric('平均年化報酬率 %', value="{} %".format(np.round(avg, 2)))    
+    a2.metric('平均年化波動率 %', value="{} %".format(np.round(std, 2)))    
+    a3.metric('持倉期間最大拉回 %', value="{} %".format(np.round(mdd, 2)))
+    a4.metric('持倉期間報酬率 %', value="{} %".format(np.round(df.loc[df.index[-1], 'cumLogReturn'] * 100, 2)))
+
 
 
 
@@ -56,15 +64,17 @@ if btn:
         'title':{'text': str(stockId), }, 
         'tooltip':{}, 
         'xAxis':{'type': 'category', 'data':[str(i) for i in df.index], }, 
-        'yAxis':[{'type':'value', }], 
+        'yAxis':[{'type':'value', 'position':'left'}, ], 
         'series':[
             {'data':[i for i in df['Adj Close']], 'type':'line', }, 
-            {'data':[i for i in df['DrowDown']], 'type':'line', 'areaStyle':{}, }
+            {'data':[i for i in df['DrowDownC']], 'type':'line', 'areaStyle':{}, }
         ]
     }
     
     st_echarts(options=options) 
 
+    del df['DrowDownC']
+    df = df.round(2)
     st.write(df)
 
 
