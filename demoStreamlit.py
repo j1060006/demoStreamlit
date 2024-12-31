fcd5f36eeec48ec27b8788eded15466ff9163081
j1060006 <j1060006@gmail.com>
2024-12-31 23:42:49 +0800

    add

--- demoStreamlit.py
+++ demoStreamlit.py
@@ -29,34 +29,59 @@ if btn:
     df.index.name = 'Date' 
     
     
-    df['logReturn'] = np.log(df['Adj Close'] / df['Adj Close'].shift(1))          
-    df['logReturn'] = df['logReturn'].fillna(0)
-    df['cumLogReturn'] = df['logReturn'].cumsum(axis=0)
-    avg = df['logReturn'].mean() * 252 * 100
-    std = np.sqrt(252) * np.std(df['logReturn']) * 100
+    # df['logReturn'] = np.log(df['Adj Close'] / df['Adj Close'].shift(1))          
+    # df['logReturn'] = df['logReturn'].fillna(0)
+    # df['cumLogReturn'] = df['logReturn'].cumsum(axis=0)
+    # avg = df['logReturn'].mean() * 252 * 100
+    # std = np.sqrt(252) * np.std(df['logReturn']) * 100
     
 
+    # dd = []
+    # for i in range(df.shape[0]):
+    #     hh = df.loc[df.index[:i+1], 'logReturn'].max()
+    #     dd.append(df.loc[df.index[i], 'logReturn'] - hh)
+    # df['DrowDown'] = dd  
+    # mdd = (df['DrowDown'].min()) * 100    
+    # df['ProfitLoss'] = initCapital * df['logReturn'].cumsum(axis=0)
+
+    # ddc = []
+    # for i in range(df.shape[0]):
+    #     hhc = df.loc[df.index[:i+1], 'Adj Close'].max()
+    #     ddc.append(df.loc[df.index[i], 'Adj Close'] - hhc)
+    # df['DrowDownC'] = ddc  
+
+
+    arr = np.log(df['Adj Close']/df['Adj Close'].shift(1))
+    arr = arr.fillna(0)
+    avg = arr.mean() * 252 * 100
+    std = arr.std() * np.sqrt(252) * 100
+    
     dd = []
     for i in range(df.shape[0]):
-        hh = df.loc[df.index[:i+1], 'logReturn'].max()
-        dd.append(df.loc[df.index[i], 'logReturn'] - hh)
-    df['DrowDown'] = dd  
-    mdd = (df['DrowDown'].min()) * 100    
-    df['ProfitLoss'] = initCapital * df['logReturn'].cumsum(axis=0)
+        hh = df.loc[df.index[:i+1], 'Adj Close'].max()
+        dd.append(df.loc[df.index[i], 'Adj Close'] - hh)
 
-    ddc = []
+    mdd = np.min(dd)
+
+    arrC = df['Adj Close'] / df.loc[df.index[0], 'Adj Close'] - 1 
+    df['cumReturn'] = arrC
+    ddC = []
     for i in range(df.shape[0]):
-        hhc = df.loc[df.index[:i+1], 'Adj Close'].max()
-        ddc.append(df.loc[df.index[i], 'Adj Close'] - hhc)
-    df['DrowDownC'] = ddc  
+        hhC = df.loc[df.index[:i+1], 'cumReturn'].max()
+        ddC.append(df.loc[df.index[i], 'cumReturn'] - hhC)
+    
+
+    
+    df['DrowDownPrice'] = dd
+    mddC = np.min(ddC) * 100
 
 
     a1, a2, a3, a4= st.columns(4)
     a1.metric('平均年化報酬率 %', value="{} %".format(np.round(avg, 2)))    
     a2.metric('平均年化波動率 %', value="{} %".format(np.round(std, 2)))    
-    a3.metric('持倉期間最大拉回 %', value="{} %".format(np.round(mdd, 2)))
-    a1.metric('持倉期間獲利 USD', value="$ {}".format(np.round(df.loc[df.index[-1], 'cumLogReturn'] * initCapital, 2)))
-
+    a3.metric('持倉期間最大拉回 %', value="{} %".format(np.round(mddC, 2)))
+    # a1.metric('持倉期間獲利 USD', value="$ {}".format(np.round(df.loc[df.index[-1], 'cumLogReturn'] * initCapital, 2)))
+    a1.metric('持倉期間獲利 USD', value="$ {}".format(np.round(df.loc[df.index[-1], 'cumReturn'] * initCapital, 2)))
 
 
 
@@ -67,13 +92,14 @@ if btn:
         'yAxis':[{'type':'value', 'position':'left'}, ], 
         'series':[
             {'data':[i for i in df['Adj Close']], 'type':'line', }, 
-            {'data':[i for i in df['DrowDownC']], 'type':'line', 'areaStyle':{}, }
+            # {'data':[i for i in df['DrowDownC']], 'type':'line', 'areaStyle':{}, }
+            {'data':[i for i in df['DrowDownPrice']], 'type':'line', 'areaStyle':{}, }
         ]
     }
     
     st_echarts(options=options) 
 
-    del df['DrowDownC']
+    # del df['DrowDownC']
     df = df.round(2)
     st.write(df)
 
